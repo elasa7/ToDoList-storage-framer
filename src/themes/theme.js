354c16e54342f3a1233455ced07766b51eaf45e0
Rawski Przemyslaw <elasa7@gmail.com>
2020-11-13 17:30:12 +0100

    finish info label - update icon

---
+++ src/themes/theme.js
@@ -0,0 +1,17 @@
+export const lightTheme = {
+  mainBg: `#FFF8ED`,
+  primaryColor: "#3F3931",
+  secondaryColor: "#FF4000",
+  bgTaskColor: "#ECE3D6",
+  taskBtnBgColor: "#FFF8ED",
+  iconColor: "#3F3931",
+};
+
+export const darkTheme = {
+  mainBg: `#28241E`,
+  primaryColor: "#3F3931",
+  secondaryColor: "#FF4000",
+  bgTaskColor: "#3F3931",
+  taskBtnBgColor: "#FFF8ED",
+  iconColor: "#FFF8ED",
+};
