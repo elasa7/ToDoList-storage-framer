668caa7ba874b5afc544cde662682d94e8dc00f5
Rawski Przemyslaw <elasa7@gmail.com>
2020-11-13 14:08:03 +0100

    add icon to button

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,4 +1,6 @@
 import React from "react";
+import DeleteIcon from "../img/delete_icon.svg";
+import EditIcon from "../img/edit_icon.svg";
 
 const TaskItem = ({ taskTitle, handleDelete, handleEdit, id }) => {
   return (
@@ -8,10 +10,10 @@ const TaskItem = ({ taskTitle, handleDelete, handleEdit, id }) => {
       </div>
       <div className="task__tool_wraper">
         <button name="edit" onClick={(e) => handleEdit(id)}>
-          edit
+          <img src={EditIcon} className="task__icon" alt="edit icon" />
         </button>
         <button name="delete" onClick={(e) => handleDelete(taskTitle)}>
-          delete
+          <img src={DeleteIcon} className="task__icon" alt="delete icon" />
         </button>
       </div>
     </div>
