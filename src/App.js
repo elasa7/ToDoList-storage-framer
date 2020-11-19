f1e0d583401c8fd7fce7a3e359e2a2305c11bec4
Rawski Przemyslaw <elasa7@gmail.com>
2020-11-20 00:05:25 +0100

    add animation: theme_icon - edit_name

--- src/App.js
+++ src/App.js
@@ -69,7 +69,8 @@ function App() {
                 initial="initial"
                 animate="animate"
                 exit="exit"
-                variants={taskVariant}>
+                variants={taskVariant}
+              >
                 <TaskItem
                   id={id}
                   taskTitle={tittle}
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import SunIcon from "../img/sun_icon.png";
 import MoonIcon from "../img/moon_icon.png";
 import { ReactComponent as EditIcon } from "../img/edit_icon.svg";
 import ProfileName from "./ProfileName";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Header = ({ theme, switchTheme, setSwitchTheme, taskCount, genId }) => {
   const [editName, setEditName] = useState(false);
@@ -15,23 +16,31 @@ const Header = ({ theme, switchTheme, setSwitchTheme, taskCount, genId }) => {
   const handleEditName = (e) => {
     setEditName(!editName);
   };
-  console.log(genId);
 
   return (
     <div className="header__wrap">
-      <div className="theme__switch_wrap" onClick={() => handleSwitchTheme()}>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.5 }}
+        className="theme__switch_wrap"
+        onClick={() => handleSwitchTheme()}
+      >
         <img src={switchTheme ? MoonIcon : SunIcon} alt="icon swithc" />
         <h5>theme</h5>
-      </div>
-      <div className="welcome__wrap">
-        <div className="edit__welcom_name">
+      </motion.div>
+      <motion.div className="welcome__wrap" layout>
+        <motion.div
+          whileHover={{ scale: 1.3 }}
+          whileTap={{ scale: 0.5 }}
+          className="edit__welcom_name"
+        >
           <EditIcon
             fill={theme.iconColor}
             width="32px"
             height="32px"
             onClick={handleEditName}
           />
-        </div>
+        </motion.div>
         <h1 className="hello__welcome">Hello</h1>
         {editName ? (
           <ProfileName
@@ -39,17 +48,23 @@ const Header = ({ theme, switchTheme, setSwitchTheme, taskCount, genId }) => {
             setcurrentName={setcurrentName}
           />
         ) : (
-          <h1 className="name__welcome" onClick={handleEditName}>
+          <motion.h1
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="name__welcome"
+            onClick={handleEditName}
+          >
             {currentName}
-          </h1>
+          </motion.h1>
         )}
-      </div>
+      </motion.div>
       <div className="count__task_wrap">
         <div className="task__todo_num">
           <h3>you have</h3>
           <div
             className="task__number"
-            style={{ background: theme.accentColor }}>
+            style={{ background: theme.accentColor }}
+          >
             <h2>{taskCount}</h2>
           </div>
           <h3>task to Do</h3>
