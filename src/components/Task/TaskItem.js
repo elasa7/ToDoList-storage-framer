180d94991fe52c017b7d1e0ecb54501b5a8723fb
Rawski Przemyslaw <elasa7@gmail.com>
2020-11-26 23:42:17 +0100

    change Paper to Accordion

--- src/components/Task/TaskItem.js
+++ src/components/Task/TaskItem.js
@@ -1,7 +1,16 @@
 import React from "react";
-import { Paper, Button } from "@material-ui/core";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Paper,
+  Button,
+  Typography,
+  Divider,
+} from "@material-ui/core";
 import Edit from "@material-ui/icons/Edit";
 import Delete from "@material-ui/icons/Delete";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { useTheme } from "@material-ui/core/styles";
 import style from "./taskItem.module.css";
 
@@ -9,26 +18,47 @@ const TaskItem = ({ taskTitle, handleDelete, handleEdit, id }) => {
   const theme = useTheme();
 
   return (
-    <Paper className={style.task__wraper}>
-      <Paper
-        elevation={0}
-        style={{ background: theme.palette.background.default }}
-        className={style.task__item_num}>
-        <h2>{id + 1}</h2>
-      </Paper>
-      <div className={style.task__title_wrap}>
-        <h4>{taskTitle}</h4>
-      </div>
-      <div className={style.task__tool_wraper}>
-        <Button name="Edit" onClick={(e) => handleEdit(id)}>
-          <Edit />
-        </Button>
+    <div className={style.task__wraper}>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <div className={style.task__wrap}>
+            <Paper
+              elevation={0}
+              className={style.task__item_num}
+              style={{ background: theme.palette.primary.main }}
+            >
+              <Typography variant="h5">{id + 1}</Typography>
+            </Paper>
+            <div className={style.task__title_wrap}>
+              <Typography variant="caption">20:45 20-10</Typography>
+              <Typography variant="h5">{taskTitle}</Typography>
+            </div>
 
-        <Button name="delete" onClick={handleDelete}>
-          <Delete />
-        </Button>
-      </div>
-    </Paper>
+            <div className={style.task__tool_wraper}>
+              <Button name="Edit" onClick={(e) => handleEdit(id)}>
+                <Edit />
+              </Button>
+
+              <Button name="delete" onClick={handleDelete}>
+                <Delete />
+              </Button>
+            </div>
+          </div>
+        </AccordionSummary>
+        <AccordionDetails className={style.task__summary}>
+
+            <hr/>
+          <Typography variant="body2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
   );
 };
 
--- src/themes/theme.js
+++ src/themes/theme.js
@@ -52,7 +52,8 @@ const lightTheme = createMuiTheme({
     },
   },
   typography: {
-    h2: {},
+    h2: {color:colorDark.mainContentBg},
+    body2:{color:'#777',},
   },
 });
 
